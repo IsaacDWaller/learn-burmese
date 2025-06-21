402b2556d2ee60754b65bd2328d678df01152e85
Isaac Waller <isaac.d.waller@gmail.com>
2025-06-21 18:02:34 +1000

    fix: round button colour

--- app/ui/round-button.tsx
+++ app/ui/round-button.tsx
@@ -1,7 +1,7 @@
 interface RoundButtonProps {
     colour: string,
     symbol: string,
-    onClick: () => void,
+    onClick?: () => void,
 };
 
 export default function RoundButton({
@@ -9,8 +9,13 @@ export default function RoundButton({
     symbol,
     onClick,
 }: RoundButtonProps) {
+    const colourStyles = {
+        red: "bg-red-500 hover:bg-red-400 active:bg-red-600",
+        green: "bg-green-500 hover:bg-green-400 active:bg-green-600",
+    }[colour] || "bg-gray-500 hover:bg-gray-400 active:bg-gray-600";
+
     return <button
-        className={`w-16 h-16 flex items-center justify-center text-white rounded-full cursor-pointer transition ease-in-out bg-${colour}-500 hover:bg-${colour}-400 active:bg-${colour}-600`}
+        className={`w-16 h-16 flex items-center justify-center text-white rounded-full cursor-pointer transition ease-in-out ${colourStyles}`}
         onClick={onClick}
     >
         <span
