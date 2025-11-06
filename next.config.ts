8cab92fad6acedfcd747f5aab95f7c489db2fbe0
Isaac Waller <isaac.d.waller@gmail.com>
2025-11-06 18:49:00 +1100

    build: upgrade Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: { domains: ["flagcdn.com"] },
+  reactCompiler: true,
+  images: {
+    remotePatterns: [{
+      protocol: "https",
+      hostname: "flagcdn.com",
+    }]
+  },
 };
 
 export default nextConfig;
